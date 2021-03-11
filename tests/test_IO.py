e99c297289def37f9aff7197faad632fac86c12a
J-Vernay <contact@jvernay.fr>
2021-03-11 16:33:15 -0500

    Added test_readLine_wholeFIle

--- tests/test_IO.py
+++ tests/test_IO.py
@@ -7,7 +7,7 @@ from tempfile import gettempdir
 class TestIO(unittest.TestCase):
         
     def test_readLine(self):
-        # This test compares the output of IO_readLine with the output of Python's .readlines
+        # This test compares individual lines retrieved with IO_readLine and Python's .readlines
         
         # first let's read lines with Python
         with open("wrapper.py", "rb") as f:
@@ -26,7 +26,18 @@ class TestIO(unittest.TestCase):
                 array.end = array.begin # reset array
             # no more lines shall be available
             self.assertFalse(lib.IO_readLine(f, array))
-     
+    
+    def test_readLine_wholeFile(self):
+        # This test compares entire file read by IO_readLine and Python's read
+        with open("test_IO.py", "rb") as f:
+            expected = f.read() + b"\n"
+        
+        with lib.DynArray_make(10) as array, lib.IO_openIn(b"test_IO.py") as f:
+            while lib.IO_readLine(f, array):
+                # removing last '\0' so that all lines are concatenated
+                array.end -= 1
+            result = cast(array.begin, c_char_p).value
+            self.assertEqual(result, expected)
         
         
 if __name__ == "__main__":
